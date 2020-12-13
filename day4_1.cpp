8a5b677d92da4f5a3f237f294cc5ef890205c833
Nathanael Tehilla <givenathananapple@gmail.com>
2020-12-13 01:17:00 -0800

    day 4

--- day4_1.cpp
+++ day4_1.cpp
@@ -3,28 +3,23 @@
 using namespace std;
 
 int main(){
-	string data, type, content;
-	set<string> types;
+	string data;
+	int size = 0, valid = 0;
 	bool haveCid = false;
-	int setSize, sizeNow, valid = 0;
-
-	while(cin >> data){
-		replace(data.begin(), data.end(), ':', ' ');
-		stringstream ss(data);
-		ss >> type >> content;
-		setSize = types.size();
-
-		if (type == "cid") haveCid = true;
-		types.insert(type);
-		sizeNow = types.size();
-
-		if (sizeNow == setSize){
-			if (sizeNow == 8) valid++;
-			else if (sizeNow == 7 && !haveCid) valid++;
-
-			types.clear();
-			types.insert(type);
+	while(getline(cin, data)){
+		if(data.size()){
+			string type, content;
+			replace(data.begin(), data.end(), ':', ' ');
+			stringstream ss(data);
+			while(ss >> type){
+				ss >> data;
+				if(type=="cid") haveCid = true;
+				size++;
+			}
+		}else {
+			if(size == 8 || (size==7 && !haveCid)) valid++;
 			haveCid = false;
+			size = 0;
 		}
 	}
 
